9de6b010b5e7624c8967e56a8a8c6a9c0abc5739
Damian Badura <damian.badura@sap.com>
2022-12-30 15:29:11 +0100

    unify names

--- internal/webhook/config.go
+++ internal/webhook/config.go
@@ -2,8 +2,8 @@ package webhook
 
 type Config struct {
 	SystemNamespace string `envconfig:"default=default"`
-	ServiceName     string `envconfig:"default=warden-webhook"`
-	SecretName      string `envconfig:"default=warden-webhook"`
+	ServiceName     string `envconfig:"default=warden-admission"`
+	SecretName      string `envconfig:"default=warden-admission-cert"`
 	Port            int    `envconfig:"default=8443"`
 	ConfigPath      string `envconfig:"default=/appdata/config.yaml"`
 }
