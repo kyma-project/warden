932c2039219be5eda06d608c8f7294b6055c201c
Damian Badura <damian.badura@sap.com>
2023-05-25 11:53:36 +0200

    add watcher

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -50,7 +50,6 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-
 }
 
 func main() {
@@ -82,6 +81,11 @@ func main() {
 		os.Exit(10)
 	}
 	logger := l.WithContext()
+	if err := config.Watch(configPath, logger.Named("config watcher")); err != nil {
+		setupLog.Error(err, "while setup file watcher")
+		os.Exit(2)
+	}
+
 	logrZap := zapr.NewLogger(logger.Desugar())
 	ctrl.SetLogger(logrZap)
 
