870abee6568aa80be3f4c556dee1c22bb1acb655
Piotr Halama <piotr.halama@sap.com>
2024-10-16 08:51:57 +0200

    Fix warden status check

--- hack/verify_warden_status.sh
+++ hack/verify_warden_status.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-function get_kyma_status () {
-	local number=1
-	while [[ $number -le 100 ]] ; do
-		echo ">--> checking warden deployment status #$number"
-		local STATUS=$(kubectl get deployment warden-operator -n kyma-system -o jsonpath='{.status.conditions[0].status}')
-		echo "warden ready: ${STATUS:='UNKNOWN'}"
-		[[ "$STATUS" == "True" ]] && return 0
-		sleep 5
-        	((number = number + 1))
-	done
-
+get_all_and_fail() {
 	kubectl get all --all-namespaces
 	exit 1
 }
 
-get_kyma_status
+echo "waiting for deployment"
+kubectl wait -n kyma-system --for=condition=Available --timeout=1m deployment warden-operator
+if [[ $? -ne 0 ]]; then
+	get_all_and_fail
+fi
+
+echo "waiting for operator"
+kubectl wait -n kyma-system --for=condition=Ready --timeout=1m pod --selector "app.kubernetes.io/component"="warden-operator"
+if [[ $? -ne 0 ]]; then
+	get_all_and_fail
+fi
+
+echo "waiting for admission"
+kubectl wait -n kyma-system --for=condition=Ready --timeout=1m pod --selector "app.kubernetes.io/component"="warden-admission"
+if [[ $? -ne 0 ]]; then
+	get_all_and_fail
+fi
